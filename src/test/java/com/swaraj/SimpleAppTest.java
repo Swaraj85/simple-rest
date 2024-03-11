010910188f3a4b6c8a6f4152c661ecdd7afd5712
swaraj <ghoshswaraj2k@gmail.com>
2024-03-11 18:47:37 +0530

    added unit test

--- src/test/java/com/swaraj/SimpleAppTest.java
+++ src/test/java/com/swaraj/SimpleAppTest.java
@@ -1,7 +1,16 @@
 package com.swaraj;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@Slf4j
 class SimpleAppTest {
 
+    @Test
+    void basic_test() {
+        log.info("executing tests");
+        assertEquals(true, true);
+    }
 }
